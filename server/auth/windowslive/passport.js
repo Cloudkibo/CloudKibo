10e630d2c09af3eb354a341763368402fa5d39ef
sabachanna <shabir.saba@gmail.com>
2014-12-03 13:27:45 -0800

    hotmail debugging

--- server/auth/windowslive/passport.js
+++ server/auth/windowslive/passport.js
@@ -22,7 +22,7 @@ exports.setup = function (User, config) {
             email: profile.emails[0].value,
             role: 'user',
             windows_photo: profile.photos[0].value,
-            username: profile.username,
+            username: profile.emails[0].value.split('@')[0],
             provider: 'windowslive',
             windowslive: profile._json
           });
