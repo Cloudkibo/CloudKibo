c7e998a326a76d6157aafab353738be3b5201687
sabachanna <sojharo@gmail.com>
2015-01-02 03:49:11 +0500

    debugged presence bug

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -22,27 +22,33 @@ function onDisconnect(socketio,socket) {
 		if(nickname != null){
 		
 				user.findOne({username : nickname}, function(err, gotuser){
-				var contactslist = require('./../api/contactslist/contactslist.model.js');
-				
-				contactslist.find({userid : gotuser._id}).populate('contactid').exec(function(err3, gotContactList){
-				console.log(gotContactList);	
-					if(gotContactList != null){
-						var i = 0;
-						clients.forEach(function(client) {
-							client.get('nickname', function(err, nickname2) {
-								for(var j in gotContactList){
-									if(nickname2 == gotContactList[j].contactid.username){
-										socketid = client.id;
-										socketio.sockets.socket(socketid).emit('offline', gotuser);
-									}
-								}
-								i++;
-							})
-						});
-
-					}
 					
-				})
+					if(gotuser != null){
+						
+						var contactslist = require('./../api/contactslist/contactslist.model.js');
+				
+						contactslist.find({userid : gotuser._id}).populate('contactid').exec(function(err3, gotContactList){
+						console.log(gotContactList);	
+							if(gotContactList != null){
+								var i = 0;
+								clients.forEach(function(client) {
+									client.get('nickname', function(err, nickname2) {
+										for(var j in gotContactList){
+											if(nickname2 == gotContactList[j].contactid.username){
+												socketid = client.id;
+												socketio.sockets.socket(socketid).emit('offline', gotuser);
+											}
+										}
+										i++;
+									})
+								});
+
+							}
+							
+						})
+						
+					}
+				
 
 			});
 				
