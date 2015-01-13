b7cc458b8eab3bc9f809ab90d7c03dce46f45b48
sabachanna <shabir.saba@gmail.com>
2015-01-13 17:44:56 +0500

    Meeting revised

--- client/app/main/meeting.controller.js
+++ client/app/main/meeting.controller.js
@@ -51,6 +51,12 @@ angular.module('cloudKiboApp')
 	$scope.closeAlertCallStart = function(index) {
 		$scope.alertsCallStart.splice(index, 1);
 	};
+
+	$scope.extensionAvailable = false;
+
+	$scope.showExtension = function(){
+		return $scope.extensionAvailable;
+	};
 	
 	$scope.localCameraCaptured = function () {
 		return $scope.localCameraOn;
@@ -678,15 +684,137 @@ angular.module('cloudKiboApp')
 	 ////////////////////////////////////////////////////////////////////////////////////////
 	// Screen Sharing Logic                                                               //
 	///////////////////////////////////////////////////////////////////////////////////////
-	 
-	 var screen_constraints = {video: { mandatory: { chromeMediaSource: 'screen' } }};
+
+		// todo: need to check exact chrome browser because opera also uses chromium framework
+		var isChrome = !!navigator.webkitGetUserMedia;
+
+		// DetectRTC.js - https://github.com/muaz-khan/WebRTC-Experiment/tree/master/DetectRTC
+		// Below code is taken from RTCMultiConnection-v1.8.js (http://www.rtcmulticonnection.org/changes-log/#v1.8)
+		var DetectRTC = {};
+
+		(function () {
+			var screenCallback;
+
+			DetectRTC.screen = {
+				chromeMediaSource: 'screen',
+				getSourceId: function (callback) {
+					if (!callback) throw '"callback" parameter is mandatory.';
+					screenCallback = callback;
+					window.postMessage('get-sourceId', '*');
+				},
+				isChromeExtensionAvailable: function (callback) {
+					if (!callback) return;
+
+					if (DetectRTC.screen.chromeMediaSource == 'desktop') callback(true);
+
+					// ask extension if it is available
+					window.postMessage('are-you-there', '*');
+
+					setTimeout(function () {
+						if (DetectRTC.screen.chromeMediaSource == 'screen') {
+							callback(false);
+						} else callback(true);
+					}, 2000);
+				},
+				onMessageCallback: function (data) {
+					console.log('chrome message', data);
+
+					// "cancel" button is clicked
+					if (data == 'PermissionDeniedError') {
+						DetectRTC.screen.chromeMediaSource = 'PermissionDeniedError';
+						if (screenCallback) return screenCallback('PermissionDeniedError');
+						else throw new Error('PermissionDeniedError');
+					}
+
+					// extension notified his presence
+					if (data == 'kiboconnection-extension-loaded') {
+						DetectRTC.screen.chromeMediaSource = 'desktop';
+					}
+
+					// extension shared temp sourceId
+					if (data.sourceId) {
+						DetectRTC.screen.sourceId = data.sourceId;
+						if (screenCallback) screenCallback(DetectRTC.screen.sourceId);
+					}
+				}
+			};
+
+			// check if desktop-capture extension installed.
+			if (window.postMessage && isChrome) {
+				DetectRTC.screen.isChromeExtensionAvailable(function(status){
+					$scope.extensionAvailable = !status;
+				});
+			}
+		})();
+
+
+		window.addEventListener('message', function (event) {
+			if (event.origin != window.location.origin) {
+				return;
+			}
+
+			//console.log('THIS IS THE EVENT')
+			//console.log(event)
+
+			DetectRTC.screen.onMessageCallback(event.data);
+		});
+
+		//-----------------//
+
+		function captureUserMedia(onStreamApproved) {
+			// this statement defines getUserMedia constraints
+			// that will be used to capture content of screen
+			var screen_constraints = {
+				mandatory: {
+					chromeMediaSource: DetectRTC.screen.chromeMediaSource,
+					maxWidth: 1920,
+					maxHeight: 1080,
+					minAspectRatio: 1.77
+				},
+				optional: []
+			};
+
+			// this statement verifies chrome extension availability
+			// if installed and available then it will invoke extension API
+			// otherwise it will fallback to command-line based screen capturing API
+			if (DetectRTC.screen.chromeMediaSource == 'desktop' && !DetectRTC.screen.sourceId) {
+				DetectRTC.screen.getSourceId(function (error) {
+					// if exception occurred or access denied
+					if (error && error == 'PermissionDeniedError') {
+						alert('PermissionDeniedError: User denied to share content of his screen.');
+					}
+
+					captureUserMedia(onStreamApproved);
+				});
+				return;
+			}
+
+			console.log(DetectRTC.screen.chromeMediaSource)
+
+			// this statement sets gets 'sourceId" and sets "chromeMediaSourceId"
+			if (DetectRTC.screen.chromeMediaSource == 'desktop') {
+				screen_constraints.mandatory.chromeMediaSourceId = DetectRTC.screen.sourceId;
+			}
+
+			// it is the session that we want to be captured
+			// audio must be false
+			var session = {
+				audio: false,
+				video: screen_constraints
+			};
+
+			// now invoking native getUserMedia API
+			navigator.webkitGetUserMedia(session, onStreamApproved, OnStreamDenied);
+
+		}
 	 
 	 $scope.showScreenText = 'Share Screen';
 	 
 	 $scope.showScreen = function(){
 		 
 		 if($scope.showScreenText == 'Share Screen'){
-			getUserMedia(screen_constraints, handleUserMediaShowScreen, handleUserMediaErrorShowScreen)
+			//getUserMedia(screen_constraints, handleUserMediaShowScreen, handleUserMediaErrorShowScreen)
+			 captureUserMedia(onStreamApproved);
 		 }
 		 else{
 			 if(localStreamScreen){
@@ -728,7 +856,7 @@ angular.module('cloudKiboApp')
 	     
 	 }
 	 
-	 function handleUserMediaShowScreen(newStream){
+	 function onStreamApproved(newStream){
 		
 		var localvideoscreen = document.getElementById("localvideoscreen");
 		localvideoscreen.src = URL.createObjectURL(newStream);
@@ -769,14 +897,33 @@ angular.module('cloudKiboApp')
 		
 	 }
 	   
-	 function handleUserMediaErrorShowScreen(error){
+	 function OnStreamDenied(error){
 		//console.log(error);
-		$scope.addAlertCallStart('danger', 'If you tried to share '+
-									'screen you must enable the option "Enable screen capture support in getUserMedia()." '+
-									'from chrome://flags. Type chrome://flags in separate tab of your Chrome browser to enable the option.');
+		$scope.addAlertCallStart('danger', error);
 									
 	 }
-	 
+
+
+	$scope.installExtension= function(){
+
+		!!navigator.webkitGetUserMedia
+		&& !!window.chrome
+		&& !!chrome.webstore
+		&& !!chrome.webstore.install &&
+		chrome.webstore.install(
+			'https://chrome.google.com/webstore/detail/hjfejjmhpakdodimneibbmgfhfhjedod',
+			successInstallCallback,
+			failureInstallCallback
+		);
+
+	}
+
+	function successInstallCallback() {
+		location.reload();
+	}
+	function failureInstallCallback(error) {
+		alert(error);
+	}
 	 
 	 ////////////////////////////////////////////////////////////////////////////////////////
 	// WebRTC DataChannel logic (Text Messages)                                           //
