db16849025cffe7e08f6be5a70353ffcf1771860
sojharo <sojharo@gmail.com>
2015-08-01 21:36:40 +0500

    5th person conference issue 123 number of people in conference 98

---
+++ client/components/conference/video.directive.js
@@ -0,0 +1,48 @@
+/**
+ * Created by sojharo on 8/1/2015.
+ */
+'use strict';
+
+angular.module('cloudKiboApp')
+  .directive('videoPlayer', function ($sce) {
+    return {
+      template: '<div class="videoBoxContainer">' +
+      '<div class="hideVideoBox" ng-hide="{{hasSharedVideo()}}"></div>' +
+      '<video ng-src="{{trustSrc()}}" autoplay width="170px" ng-show="{{hasSharedVideo()}}" class="videoelement"></video>' +
+      '<span>{{peerUserName()}}</span>' +
+      '<audio autoplay ng-src="{{trustAudSrc()}}"></audio>' +
+      '</div>',
+      restrict: 'E',
+      replace: true,
+      scope: {
+        vidSrc: '@',
+        audSrc: '@',
+        userName: '@',
+        sharedVid: '@'
+      },
+      link: function (scope) {
+        console.log('Initializing video-player');
+        scope.trustSrc = function () {
+          if (!scope.vidSrc) {
+            return undefined;
+          }
+          return $sce.trustAsResourceUrl(scope.vidSrc);
+        };
+        scope.hasSharedVideo = function () {
+          return scope.sharedVid;
+        };
+        scope.trustAudSrc = function () {
+          if (!scope.audSrc) {
+            return undefined;
+          }
+          return $sce.trustAsResourceUrl(scope.audSrc);
+        };
+        scope.peerUserName = function () {
+          if (!scope.userName) {
+            return undefined;
+          }
+          return $sce.trustAsResourceUrl(scope.userName);
+        };
+      }
+    };
+  });
