6aa22179dc8ce48e5c6da370410cb1a9f089e8bb
sabachanna <shabir.saba@gmail.com>
2015-08-02 18:03:43 +0500

    separating client log file from server log file 114

--- server/components/logger/logger.js
+++ server/components/logger/logger.js
@@ -1,3 +1,56 @@
 var fs = require('fs');
 
 
+var dir = __dirname + "/../log";
+var clientFile = dir + '/client';
+var serverFile = dir + '/server';
+
+exports.clientLog = function(data) {
+
+  fs.readFile(clientFile, 'utf8', function (err, configFile) {
+    if (err) {
+      console.log('Error: ' + err);
+      return;
+    }
+
+    configFile += ''+ Date.now() +': '+ data +'\n';
+
+    fs.writeFile(clientFile, configFile, function (err) {
+      if (err) {
+        console.log('Error: ' + err);
+        return;
+      }
+
+      fs.close(clientFile, function(){
+        console.log('file is updated');
+      });
+
+    });
+
+  });
+};
+
+exports.serverLog = function(data) {
+
+  fs.readFile(serverFile, 'utf8', function (err, configFile) {
+    if (err) {
+      console.log('Error: ' + err);
+      return;
+    }
+
+    configFile += ''+ Date.now() +': '+ data +'\n';
+
+    fs.writeFile(serverFile, configFile, function (err) {
+      if (err) {
+        console.log('Error: ' + err);
+        return;
+      }
+
+      fs.close(serverFile, function(){
+        console.log('file is updated');
+      });
+
+    });
+
+  });
+};
