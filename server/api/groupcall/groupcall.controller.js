41c92e63013eb2bf1173f11a9c964805ff079e5c
sabachanna <shabir.saba@gmail.com>
2015-09-21 04:31:45 +0500

    server logic for group call 208

--- server/api/groupcall/groupcall.controller.js
+++ server/api/groupcall/groupcall.controller.js
@@ -54,6 +54,98 @@ exports.destroy = function(req, res) {
   });
 };
 
+exports.addcontacttogroup = function(req, res) {
+  User.findById(req.user._id, function (err, gotUser) {
+    if (err) return console.log('Error 1'+ err);
+
+    User.findOne({username : req.body.searchusername}, function (err, gotUserSaved) {
+
+      console.log("Add user to group")
+      if(gotUserSaved == null)
+        return res.send({status: 'success', msg: null});
+
+      grouplist.count({userid : gotUser._id, contactid : gotUserSaved._id}, function(err5, gotCount){
+
+        if(gotUser.username == gotUserSaved.username)
+          res.send({status: 'danger', msg: 'You can not add your self as a group.'});
+        else if(gotCount > 0)
+          res.send({status: 'danger', msg: gotUserSaved.username +' is already added in your group list with name '+ gotUserSaved.firstname +' '+ gotUserSaved.lastname});
+        else{
+
+          configuration.findOne({}, function (err, gotConfig) {
+            if(err) return console.log(err);
+
+            grouplist.count({userid : gotUser._id}, function(err, gotFullCount){
+              if(err) return console.log(err);
+
+              if(gotConfig.numberofpeopleingrouplist === gotFullCount){
+                logger.serverLog("warn", "groupmember list full");
+                res.send({status: 'danger', msg: 'Your groupmember list is full.'});
+
+              }
+              else{
+                var groupmember = new grouplist({
+                  userid : gotUser._id,
+                  groupid : gotUserSaved._id
+                });
+
+                groupmember.save(function(err2){
+                  if (err2) return console.log('Error 2'+ err);
+                  grouplist.find({userid : gotUser._id}).populate('contactid').exec(function(err3, gotGroupList){
+                    res.send({status: 'success', msg: gotGroupList});
+                  })
+                })
+
+              }
+
+            });
+
+          });
+
+
+        }
+        console.log("contact add by username");
+      })
+
+    })
+  })
+};
+
+
+exports.removecontactfromgroup = function(req, res) {
+
+  logger.serverLog('info', 'grouplist.controller : The data sent by client: '+ JSON.stringify(req.body));
+
+  console.log("Removing from group list request")
+  User.findById(req.user._id, function (err, gotUser) {
+    if (err) return console.log('Error 1'+ err);
+
+    User.findOne({username : req.body.username}, function (err, gotUserSaved) {
+      grouplist.remove({userid : gotUserSaved._id, groupid : gotUser._id}, function(err6){
+        console.log("Is in group list")
+        grouplist.remove({userid : gotUser._id, groupid : gotUserSaved._id}, function(err6){
+          console.log("Is in my list")
+
+          userchat.remove({$or: [ { to : gotUserSaved.username, from : gotUser.username },
+              { to : gotUser.username, from : gotUserSaved.username } ]},
+            function(err1){
+              if(err1) return console.log(err1);
+
+              res.send({status: 'success', msg: 'Friend is removed'});
+
+              logger.serverLog('info', 'grouplist.controller : Friend removed from grouplist');
+
+            })
+
+        })
+
+      })
+
+    })
+  })
+};
+
+
 function handleError(res, err) {
   return res.send(500, err);
 }
--- server/api/groupcall/index.js
+++ server/api/groupcall/index.js
@@ -11,5 +11,7 @@ router.post('/', auth.isAuthenticated(), controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.update);
 router.patch('/:id', auth.isAuthenticated(), controller.update);
 router.delete('/:id', auth.isAuthenticated(), controller.destroy);
+router.post('/', auth.isAuthenticated(), controller.add);
+router.post('/', auth.isAuthenticated(), controller.remove);
 
 module.exports = router;
