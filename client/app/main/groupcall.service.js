1fadfc4e0f0c003f5df890166ebd746704915ecc
sabachanna <sabachanna@users.noreply.github.com>
2015-10-01 05:53:25 +0500

    GroupCall client side webrtc 207

---
+++ client/app/main/groupcall.service.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+angular.module('cloudKiboApp')
+  .factory('GroupCalllService', function ($rootScope, $q, socket, $log, logger) {
+
+    function addHandlers(socket) {
+      socket.on('groupmemberisoffline', function (nickname) {
+        $log.info('Member is OFFLINE');
+        api.trigger('groupmemberisoffline', [nickname]);
+      });
+      socket.on('groupmemeberisbusy', function (data) {
+        $log.info('Group member is BUSY');
+        api.trigger('groupmemeberisbusy', [data]);
+      });
+      socket.on('groupmembersideringing', function (data) {
+        $log.info('Callee is ringing on other side');
+        api.trigger('groupmemebersideringing', [data]);
+      });
+      socket.on('areyoufreeforcall', function (data) {
+        api.trigger('areyoufreeforcall', [data]);
+      });
+      socket.on('message', function (data) {
+        api.trigger('message', [data]);
+      });
+    }
+
+    var api = {
+      sendMessage: function (m, d) {
+        socket.emit(m, d);
+      }
+    };
+    EventEmitter.call(api);
+    Object.setPrototypeOf(api, EventEmitter.prototype);
+
+    addHandlers(socket);
+    return api;
+  });
