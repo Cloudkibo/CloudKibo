a73666dfe3119b9e91e735ac36aaed192e2a4c5c
sabachanna <shabir.saba@gmail.com>
2015-10-14 04:35:32 +0500

    reset pasword 220 group call 207

--- server/api/groupcall/index.js
+++ server/api/groupcall/index.js
@@ -6,6 +6,7 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
+router.get('/getothersgroups', auth.isAuthenticated(), controller.showothergroups);
 router.get('/', auth.isAuthenticated(), controller.index); // /api/groupcall/
 router.get('/:id', auth.isAuthenticated(), controller.show); // /api/groupcall/:id
 router.post('/', auth.isAuthenticated(), controller.create);
@@ -15,4 +16,5 @@ router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 router.post('/addcontact', auth.isAuthenticated(), controller.addcontact);
 router.post('/removecontact', auth.isAuthenticated(), controller.removecontact);
 router.get('/groupmembers/:id', auth.isAuthenticated(), controller.showgroupmembers);
+
 module.exports = router;
