53534bd2b6156e3f1e713c898eda91833763cda9
sabachanna <shabir.saba@gmail.com>
2015-11-23 19:19:58 +0500

    screen sharing 263

--- client/components/conference/conference.controller.js
+++ client/components/conference/conference.controller.js
@@ -295,14 +295,17 @@ angular.module('cloudKiboApp')
     function shareScreenUsingChromeExtension(cb) {
 
       if($scope.hasChromeExtension()) {
+        console.log('Chrome extension found installed')
         // this statement verifies chrome extension availability
         // if installed and available then it will invoke extension API
         // otherwise it will fallback to command-line based screen capturing API
         if (ScreenShare.getChromeMediaSource() == 'desktop' && !ScreenShare.getSourceIdValue()) {
+          console.log('There is not source Id value')
           ScreenShare.getSourceId(function (error) {
+            console.log(error)
             // if exception occurred or access denied
             if (error && error == 'PermissionDeniedError') {
-              alert('PermissionDeniedError: User denied to share content of his/her screen.');
+              //alert('PermissionDeniedError: User denied to share content of his/her screen.');
               logger.log('PermissionDeniedError: User denied to share content of his/her screen. Shown to: ' + $scope.user.username);
             }
             // this statement sets gets 'sourceId" and sets "chromeMediaSourceId"
