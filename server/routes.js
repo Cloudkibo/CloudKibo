1f531bf4b464cca52767c28ceeee6857002ca75c
sabachanna <shabir.saba@gmail.com>
2015-12-25 05:51:42 +0500

    contact us email 246

--- server/routes.js
+++ server/routes.js
@@ -156,7 +156,7 @@ module.exports = function(app) {
   // in NodeJS/Express (server)
   app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
     next();
 
