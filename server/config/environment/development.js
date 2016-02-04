02cb08ad33747635f4aa1c02216c14296d5c17c8
sojharo <sojharo@gmail.com>
2016-02-04 19:05:35 +0500

    merge issue

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,13 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-<<<<<<< HEAD
-    //uri: 'mongodb://cloudkibo:a345rq98efw@localhost/cloudkibo'
-    uri: 'mongodb://localhost/cloudkibo-test'
-=======
     uri: 'mongodb://cloudkibo:a345rq98efw@localhost/cloudkibo'
-  //  uri: 'mongodb://localhost/cloudkibo-test'
->>>>>>> e400e9798230ee3a79f700754c425c88305b5f01
+    //uri: 'mongodb://localhost/cloudkibo-test'
   },
 
   seedDB: false
