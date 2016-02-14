ed8e6dcd54f758a4143e0a7e8ebe20e460c95f13
zarmeen92 <zarmeennasim@yahoo.com>
2016-02-14 17:33:24 +0500

    calendar added

--- client/frontend/layout/js/clock.js
+++ client/frontend/layout/js/clock.js
@@ -1,8 +1,16 @@
+
 /*
  * Starts any clocks using the user's local time
  * From: cssanimation.rocks/clocks
  */
 window.onload = function(){
+  
+  $('#calendar').datepicker({
+        inline: true,
+        firstDay: 1,
+        showOtherMonths: true,
+        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    });
   initLocalClocks();
   moveSecondHands();
   setUpMinuteHands();
