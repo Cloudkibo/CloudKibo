ec480a20dbd2573d8ada6bd5085d028c93195cc0
sojharo <sojharo@live.com>
2016-03-14 03:26:13 +0500

    callstats.io

---
+++ client/stats.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sojharo on 3/14/16.
+ */
+var callStats = new callstats(null,io,jsSHA);
+var AppID     = "199083144";
+var AppSecret = "t/vySeaTw5q6323+ArF2c6nEFT4=";
+callStats.initialize(AppID, AppSecret, username, function (err, msg) {
+  console.log("Initializing Status: err="+err+" msg="+msg);
+});
