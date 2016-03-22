dfae9661a910e383cfdadc5f94649f4f3c29437d
Sojharo <sojharo@live.com>
2016-03-22 11:57:02 +0500

    commented out our turn server

--- client/components/kibortc/rtcconfig.service.js
+++ client/components/kibortc/rtcconfig.service.js
@@ -15,7 +15,7 @@ angular.module('kiboRtc.services')
  * todo: Add the function addICEServer which should take JSON array or JSON object as input
  */
   .factory('pc_config', function ($log) {
-
+/*
     $(document).ready(function() {
       $.get("https://service.xirsys.com/ice",
         {
@@ -30,16 +30,16 @@ angular.module('kiboRtc.services')
           console.log("Data: " + data + "nnStatus: " + status);
         });
     });
-
+*/
     var isChrome = !!navigator.webkitGetUserMedia;
 
     return {'iceServers': [
-      {url: (isChrome
+      /*{url: (isChrome
             ? 'turn:turn.cloudkibo.com:3478?transport=udp'
             : 'turn:45.55.232.65:3478?transport=udp'), username: 'cloudkibo', credential: 'cloudkibo'},
       {url: (isChrome
             ? 'turn:turn.cloudkibo.com:3478?transport=tcp'
-            : 'turn:45.55.232.65:3478?transport=tcp'), username: 'cloudkibo', credential: 'cloudkibo'},
+            : 'turn:45.55.232.65:3478?transport=tcp'), username: 'cloudkibo', credential: 'cloudkibo'},*/
       {url: 'turn:numb.viagenie.ca:3478', username: 'support@cloudkibo.com', credential: 'cloudkibo'}
 
       /*{url: 'turn:45.55.232.65:3478?transport=udp', username: 'cloudkibo', credential: 'cloudkibo'},
