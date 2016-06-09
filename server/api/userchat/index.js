0eead049cee0e9a253a4f5b4bbee104065944449
sojharo <sojharo@live.com>
2016-06-09 13:40:03 +0500

    using new route to get chat

--- server/api/userchat/index.js
+++ server/api/userchat/index.js
@@ -8,10 +8,10 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.post('/', auth.isAuthenticated(), controller.index); //www.cloudkibo.com/api/userchat/
+router.post('/alluserchat', auth.isAuthenticated(), controller.alluserchat); //www.cloudkibo.com/api/userchat/alluserchat
 router.post('/save', auth.isAuthenticated(), controller.save);//www.cloudkibo.com/api/userchat/save
 router.post('/markasread', auth.isAuthenticated(), controller.markasread);//www.cloudkibo.com/api/userchat/markasread
 router.post('/removechathistory', auth.isAuthenticated(), controller.removechathistory);
 
 
 module.exports = router;
-
