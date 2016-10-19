14bddfee66b717f3663e36261878c027c8a5fb77
Sojharo <sojharo@live.com>
2016-10-19 10:01:50 +0500

    create group 262 add members 263

---
+++ server/api/groupchatstatus/groupchatstatus.model.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var GroupChatStatus = new Schema({
+  msg_unique_id: {type: Schema.ObjectId, ref: 'groupchats'},
+  status : String,
+  user_phone : String,
+  read_date : {type: Date, default: Date.now },
+  delivered_date : {type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('groupchatstatuses', GroupChatStatus);
---
+++ server/api/groupmessaginguser/groupmessaginguser.model.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var GroupMessagingUser = new Schema({
+  group_unique_id: {type: Schema.ObjectId, ref: 'groupmessagings'},
+  member_phone: String,
+  isAdmin: String,
+  membership_status : String,
+  date_join : {type: Date, default: Date.now },
+  date_left : Date
+});
+
+module.exports = mongoose.model('groupmessagingusers', GroupMessagingUser);
