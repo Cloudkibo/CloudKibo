829611c5afda472d3e044aa0553f1507e84f52f7
Sojharo <sojharo@live.com>
2016-10-19 16:50:27 +0500

    create group fix

--- server/api/groupchat/index.js
+++ server/api/groupchat/index.js
@@ -2,13 +2,14 @@
 
 var express = require('express');
 var controller = require('./groupchat.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
